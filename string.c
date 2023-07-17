bdbbafbd80a95c82738d32dcea56acf7de52e563
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-07-17 19:13:37 +0300

    Update function _memcpy in string.c
    This commit reverts the function to its original state before it was changed. The changes made to the function resulted to Conditional jump or move depends on uninitialised value(s).

--- string.c
+++ string.c
@@ -98,6 +98,8 @@ char *_memcpy(char *dest, const char *src, unsigned int n)
 
 	for (t = 0; t < n && src[t] != '\0'; t++)
 		dest[t] = src[t];
+	for (; t < n; t++)
+		dest[t] = '\0';
 
 	return (dest);
 }
