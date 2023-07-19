bc21d3e2c152c23322ae14a860bed5d927a548c6
kahenyamercy <kahenyamercy5@gmail.com>
2023-07-19 12:52:11 +0000

    feat: handle operator

--- main.c
+++ main.c
@@ -21,6 +21,7 @@ char *execute_helper(custom_args *argv, env_var *path, char *argV[])
 {
 	char *filepath;
 	pid_t execve_process;
+	int status;
 
 	filepath = argv->argv[0];
 	execve_process = fork();
@@ -35,16 +36,20 @@ char *execute_helper(custom_args *argv, env_var *path, char *argV[])
 		{
 			print_err(argV[0]);
 			free_struct(argv, path);
-			return (NULL);
+			exit(1);
 		}
 	}
 	else
 	{
-		wait(NULL);
+		waitpid(execve_process, &status, 0);
+		if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+			*argV[2] = 1;
+
 		free_struct(argv, path);
 	}
 	return (filepath);
 }
+
 /**
  * execute_file - Executes the file specified in lineptr.
  * @lineptr: Pointer to the string representing the file path.
@@ -113,6 +118,73 @@ char *execute_file(char *lineptr, char *argV[])
 }
 
 /**
+ * execute_command - Execute a single command and return the exit status.
+ * @command: Pointer to the string representing the command.
+ * @argV: Pointer to the command line arguments passed to main.
+ *
+ * Return: The exit status of the command.
+ */
+int execute_command(char *command, char *argV[]) 
+{
+	int exit_status = 0;
+	char *commands_delimiters = "&&||";
+
+	/* Tokenize the command using "&&" and "||" as delimiters */
+	char *subcommand = str_tok(command, commands_delimiters);
+	int logical_op = 0; /* 0: no operator, 1: &&, 2: || */
+
+	while (subcommand != NULL) 
+	{
+		char *rest = subcommand + _strlen(subcommand) + 1;
+		char *memory = NULL;
+
+		if (*rest == '&' && *(rest + 1) == '&') 
+		{
+			logical_op = 1; /* && operator */
+		} 
+		else if (*rest == '|' && *(rest + 1) == '|') 
+		{
+			logical_op = 2; /* || operator */
+		} 
+		else 
+		{
+			logical_op = 0; /* no operator */
+		}
+
+		/* Execute the current subcommand */
+		memory = execute_file(subcommand, argV);
+		if (memory == NULL) 
+		{
+			exit_status = 1;
+			if (logical_op == 1)
+			{
+				break;/*if operator && and comand fails, stop execution*/
+			}
+		} 
+		else 
+		{
+			free(memory);
+		}
+
+		/* Check for logical operators */
+		if (logical_op == 1 && exit_status == 0) 
+		{ /* && operator */
+			subcommand = str_tok(NULL, commands_delimiters);
+		} 
+		else if (logical_op == 2 && exit_status != 0) 
+		{ /* || operator */
+			subcommand = str_tok(NULL, commands_delimiters);
+		} 
+		else 
+		{
+			break; /* No logical operator or exit status doesn't match, break the loop. */
+		}
+	}
+
+	return (exit_status);
+}
+
+/**
  * free_resources - Frees the allocated resources.
  * @path: Pointer to the env_var structure.
  * @argv: Pointer to the custom_args structure.
@@ -136,7 +208,7 @@ void free_resources(env_var *path, custom_args *argv)
 
 int main(int ac, char *argV[])
 {
-	char *lineptr, *memory;
+	char *lineptr;/*memory;*/
 	size_t n = 0;
 	ssize_t num_char_read;
 	int exit_status = 0;
@@ -164,18 +236,10 @@ int main(int ac, char *argV[])
 		else
 		{
 			lineptr[num_char_read - 1] = '\0';
-			memory = execute_file(lineptr, argV);
-			if (memory == NULL)
-			{
-				exit_status = 1;
-				free(lineptr);
-				continue;
-			}
-			else if (memory != NULL)
-				exit_status = 0;
+			exit_status = execute_command(lineptr, argV);
 		}
-		free(memory);
 		free(lineptr);
 	}
 	return (0);
 }
+
