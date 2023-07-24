d80431090ed44891b6f3a498c200ec3c7df2a9ad
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-07-24 08:22:06 +0000

    Update _unsetenv to use unsetenv function

--- setenv.c
+++ setenv.c
@@ -8,7 +8,6 @@
  * @name: pointer the variable whose value is to be set/updated
  * @value: pointer to the value to be set/updated
  * @overwrite: interger condition to overwrite existing variable
- * @env: pointer to the environ (external variable)
  * Return: 0 onsuccess, -1 on failure
  */
 int _setenv(const char *name, const char *value, int overwrite)
@@ -29,32 +28,10 @@ int _setenv(const char *name, const char *value, int overwrite)
 
 int _unsetenv(const char *name)
 {
-	char *variable;
-	char **env = environ;
-	int i = 0;
+	int result;
 
-	variable = _strdup(name);
-	while (env[i] != NULL)
-	{
-		char *delim = "=\n";
-		char *token;
-		char *env_cpy = _strdup(env[i]);
-
-		token = str_tok(env_cpy, delim);
-		if (token != NULL && _strcmp(token, variable) == 0)
-		{
-			while (env[i + 1] != NULL)
-			{
-				env[i] = env[i + 1];
-				i++;
-			}
-			env[i] = NULL;
-			free(env_cpy);
-			return (0);
-		}
-		free(env_cpy);
-		free(variable);
-		i++;
-	}
-	return (-1);
+	result = unsetenv(name);
+	if (result != 0)
+		perror("unsetenv");
+	return (result);
 }
